2eeb8397e74a18f5f07bcc4bb128694ee04e10a4
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-06 13:15:58 +0200

    added checkbox methods

---
+++ cypress/integration/checkbox.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe("E2E - Actions with checkbox", () => {
+    it("Checkbox - automationpractice", () => {
+        cy.visit("/index.php?id_category=3&controller=category")
+        
+        cy.get("#ul_layered_category_0").find("input").then(checkbox => {
+            cy.get(checkbox).eq(0).check();
+            cy.get(checkbox).eq(1).check().invoke("prop", "checked").then(checked => {
+                cy.get(checked)
+            })
+        })
+
+        cy.get("#ul_layered_id_attribute_group_1").find("input").check();
+    })
+})
