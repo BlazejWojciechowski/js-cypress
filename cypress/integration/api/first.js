97eec379ac4bcbaeb80c9f5858157732d942ef8a
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-07 10:12:26 +0200

    added first Api test

---
+++ cypress/integration/api/first.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("E2E - Api tests", () => {
+    it("Verification of Api tags - automationpractice", () => {
+        cy.intercept("GET", "https://api.realworld.io/api/tags").as("TagRequest");
+        cy.visit("https://angular.realworld.io/")
+        cy.wait("@TagRequest")
+        cy.get("@TagRequest").then(res => {
+            console.log(res)
+            expect(res.response.statusCode).to.equal(200)
+            expect(res.response.body.tags).to.contain("Welcome").and.to.contain("implementations")
+        })
+    })
+
+})
